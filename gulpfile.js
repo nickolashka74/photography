03331dd128bb8ba824ae8a0ac7deab8d63de3c20
nickolashka <xy4u74@yandex.ru>
2019-11-06 22:28:02 +0500

    coplete promo, about, clients, portfolio, blog, work & footer little bit

---
+++ gulpfile.js
@@ -0,0 +1,159 @@
+"use strict";
+
+const {src, dest} = require("gulp");
+const gulp = require("gulp");
+const autoprefixer = require("gulp-autoprefixer");
+const cssbeautify = require("gulp-cssbeautify");
+const removeComments = require('gulp-strip-css-comments');
+const rename = require("gulp-rename");
+const sass = require("gulp-sass");
+const cssnano = require("gulp-cssnano");
+const rigger = require("gulp-rigger");
+const uglify = require("gulp-uglify");
+const plumber = require("gulp-plumber");
+const imagemin = require("gulp-imagemin");
+const del = require("del");
+const gulpStylelint = require("gulp-stylelint");
+const htmlmin = require('gulp-htmlmin');
+const browsersync = require("browser-sync").create();
+
+
+/* Paths */
+var path = {
+    build: {
+        html: "dist/",
+        js: "dist/js/",
+        css: "dist/css/",
+        images: "dist/img/",
+        fonts: "dist/fonts/"
+    },
+    src: {
+        html: "src/*.html",
+        js: "src/js/*.js",
+        css: "src/sass/style.scss",
+        images: "src/img/**/*.{jpg,png,svg,gif,ico,webmanifest,xml}",
+        fonts: "src/fonts/*.*"
+    },
+    watch: {
+        html: "src/*.html",
+        js: "src/js/**/*.js",
+        css: "src/sass/**/*.scss",
+        images: "src/img/**/*.{jpg,png,svg,gif,ico,webmanifest,xml}",
+        fonts: "src/fonts/*.*"
+    },
+    clean: "./dist"
+}
+
+
+
+/* Tasks */
+function browserSync(done) {
+    browsersync.init({
+        server: {
+            baseDir: "./dist/"
+        },
+        port: 3000
+    });
+}
+
+function browserSyncReload(done) {
+    browsersync.reload();
+}
+
+function html() {
+    return src(path.src.html, { base: "src/" })
+        .pipe(plumber())
+        .pipe(htmlmin({ collapseWhitespace: true }))
+        .pipe(dest(path.build.html))
+        .pipe(browsersync.stream());
+}
+
+function css() {
+    return src(path.src.css, { base: "src/sass/" })
+        .pipe(plumber())
+        .pipe(gulpStylelint({
+            reporters: [
+                {
+                    formatter: 'string',
+                    console: true
+                }
+            ]
+        }))
+        .pipe(sass())
+        .pipe(autoprefixer({
+            Browserslist: ['last 5 versions'],
+            cascade: true
+        }))
+        .pipe(cssbeautify())
+        .pipe(dest(path.build.css))
+        .pipe(cssnano({
+            zindex: false,
+            discardComments: {
+                removeAll: true
+            }
+        }))
+        .pipe(removeComments())
+        .pipe(rename({
+            suffix: ".min",
+            extname: ".css"
+        }))
+        .pipe(dest(path.build.css))
+        .pipe(browsersync.stream());
+}
+
+function js() {
+    return src(path.src.js, {base: './src/js/'})
+        .pipe(plumber())
+        .pipe(rigger())
+        .pipe(gulp.dest(path.build.js))
+        .pipe(uglify())
+        .pipe(rename({
+            suffix: ".min",
+            extname: ".js"
+        }))
+        .pipe(dest(path.build.js))
+        .pipe(browsersync.stream());
+}
+
+function images() {
+    return src(path.src.images)
+        .pipe(imagemin({
+            interlaced: true,
+            progressive: true,
+            optimizationLevel: 3
+        }))
+        .pipe(dest(path.build.images));
+}
+
+function fonts() {
+    return src(path.src.fonts)
+        .pipe(dest(path.build.fonts));
+}
+
+function clean() {
+    return del(path.clean);
+}
+
+function watchFiles() {
+    gulp.watch([path.watch.html], html);
+    gulp.watch([path.watch.css], css);
+    gulp.watch([path.watch.js], js);
+    gulp.watch([path.watch.images], images);
+    gulp.watch([path.watch.fonts], fonts);
+}
+
+const build = gulp.series(clean, gulp.parallel(html, css, js, images, fonts));
+const watch = gulp.parallel(build, watchFiles, browserSync);
+
+
+
+/* Exports Tasks */
+exports.html = html;
+exports.css = css;
+exports.js = js;
+exports.images = images;
+exports.fonts = fonts;
+exports.clean = clean;
+exports.build = build;
+exports.watch = watch;
+exports.default = watch;
